1e678af6cdc43d7a4c766b61d46034115c73e7aa
Jimmie Elvenmark <flugsio@gmail.com>
2020-06-15 01:27:50 +0200

    Increment clock with a timer interrupt
    This is a accurate enough when running.
    However since the initial value is compiled into the code
    it will be a few seconds off,
    and it will be very wrong if power is lost.
    
    This should be replaced with a RTC.

--- traglu.ino
+++ traglu.ino
@@ -17,8 +17,21 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 // TODO: this should be replaced with a Real-time clocko
 // NOTE: The generated value is corrected for the local timezone (CEST+0200)
 volatile unsigned long current_time = __TIME_UNIX__ - 2*60*60;
+volatile bool redraw = true;
 
 void setup() {
+  // setup 1 second clock interrupt
+  cli();
+  TCCR1A = 0; // set entire TCCR1A register to 0
+  TCCR1B = 0; // set entire TCCR1B register to 0
+  TCNT1  = 0; // initialize counter value to 0
+  // set compare match register to 1Hz
+  OCR1A = 15624; // = (16*10^6) / (1*1024) - 1 (must be <65536)
+  TCCR1B |= (1 << WGM12); // turn on CTC mode
+  TCCR1B |= (1 << CS12) | (1 << CS10); // Set CS12 and CS10 bits for 1024 prescaler
+  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
+  sei();
+
   Serial.begin(9600);
 
   // display could use either 0x3C or 0x3D
@@ -28,15 +41,25 @@ void setup() {
   }
 }
 
+// timer1 interrupt
+ISR(TIMER1_COMPA_vect) {
+  current_time += 1;
+  redraw = true;
+}
+
+
 void loop() {
-  display.clearDisplay();
+  if (redraw) {
+    redraw = false;
+    display.clearDisplay();
 
-  // draw current time
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println(current_time);
+    // draw current time
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.println(current_time);
 
-  display.display();
+    display.display();
+  }
   delay(100);
 }
